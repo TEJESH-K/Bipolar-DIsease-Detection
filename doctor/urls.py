51bd694b7d6bdb85f5bffa1faf75fd4f62db8ffe
unknown <k.tejesh2017@vitstudent.ac.in>
2021-03-17 15:19:19 +0530

    first commit

---
+++ doctor/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+
+    path('',views.home,name='home'),
+    path('login',views.login,name="login"),
+    path('register',views.register,name="register"),
+    path('login2',views.login2,name="login2"),
+    path('rcomplete',views.rcomplete,name="rcomplete"),
+    path('bipolar',views.bipolar,name="bipolar"),
+    path('predBipolar',views.predBipolar,name='predBipolar'),
+    path('bipolarSv',views.bipolarSv,name='bipolarSv')
+]
